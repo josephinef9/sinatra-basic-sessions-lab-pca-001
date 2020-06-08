d24ed9b5f8f08af91630de92f850b0631be8d4b2
Jo Fischer <josephinefischer9@gmail.com>
2020-06-08 14:44:46 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "I am too smart for you"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+     @item = params[:item]
+     session[:item] = params[:item]
+  end
+
 end
